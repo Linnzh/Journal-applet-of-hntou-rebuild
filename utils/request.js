7f68f67bc6d44a56b11d1b6be24ec7a40fb8ee40
Linnzh <reg.lynnzh@gmail.com>
2019-03-24 02:10:04 +0800

    添加了小程序端进行异步交流数据的方法：request()

---
+++ utils/request.js
@@ -0,0 +1,20 @@
+const app = getApp()
+
+function request(url, params) {
+  let promise = new Promise(function (resolve, reject) {
+    wx.request({
+      url: url,
+      data: params,
+      method: 'GET',
+      success: function (res) {
+        app.netWorkData.result = res.data
+        resolve();
+      }
+    })
+  });
+  return promise;
+}
+
+module.exports = {
+  request: request
+}
