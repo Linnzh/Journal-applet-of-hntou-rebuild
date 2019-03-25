b7c4b17283869aef031612f1433da82c907badab
Linnzh <reg.lynnzh@gmail.com>
2019-03-25 21:09:20 +0800

    初步整理了【专题】模块的实现

--- pages/topic/topic.js
+++ pages/topic/topic.js
@@ -13,12 +13,35 @@ Page({
   // =============================系统方法=============================
   onLoad: function (options) {
     let tid = options.tid;
-    this.setData({
-      tid: tid
-    })
+    // this.setData({
+    //   tid: tid
+    // })
+    this.specialColumn(tid)
   },
 
 
   // ==============================绑定方法============================
+
+  // 专栏文章列表
+  specialColumn(tid){
+    let url = app.globalData.baseUrl + 'column_get_articles.php';
+    let data;
+    if(wx.getStorageSync('uid')) {
+      data = {
+        tid: tid,
+        uid: wx.getStorageSync('uid')
+      };
+    } else {
+      data = {
+        tid: tid
+      };
+    }
+    promise.request(url, data).then((res)=>{
+      this.setData({
+        topic: res
+      })
+    })
+  },
+
 
 })
